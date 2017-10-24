2a1d121e763f768a34c7950dde6d8a3a46e88387
Robert Rollner <robrollner@gmail.com>
2017-10-23 22:31:58 -0600

    created controllers and orm and started burger.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,1 +1,69 @@
-const express = require('express');
+var express = require('express');
+var router = express.Router();
+
+
+var burger = require('../models/burger.js');
+
+//route to show all available in the DB
+
+router.get("/", (req, res) => {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        //hbsObject = handlebars object
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+//route to create new rows
+
+router.post("/api/burgers", (req, res) => {
+    console.log(req.params);
+
+    let burgerName = req.params.burger_name;
+    let eat = req.params.devoured;
+
+    burger.create([
+        "burgerName", "eat"
+    ], [
+        req.body.burgerName, req.body.eat
+    ], (result) => {
+        res.json({ id: result, insertId });
+    });
+});
+
+//update by ID
+
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.burger_id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+//delete rows by ID
+
+router.delete("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.burger_id;
+
+    burger.delete(condition, (result) => {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
