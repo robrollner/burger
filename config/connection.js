cd0b25c1bafa515255a6404a47097b8096f2b0e6
Robert Rollner <robrollner@gmail.com>
2017-10-28 09:54:35 -0600

    got heroku db running

--- config/connection.js
+++ config/connection.js
@@ -1,39 +1,20 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "codecamp",
-    password: "secretcode",
-    database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
-
-// var connection;
-// if (process.env.JAWSDB_URL) {
-//     //Heroku deployment
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     //local host
-//     connection = mysql.createConnection({
-//         root: 3000,
-//         host: "localhost",
-//         user: "root",
-//         password: "",
-//         database: "burgers_db",
-//     });
-// };
+const connection;
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    //local host
+    connection = mysql.createConnection({
+        root: 3306,
+        host: "wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "y98qhc3qjl80d0kb",
+        password: "bchk7ahcng5roi57",
+        database: "hlkxxdv0jwz3kse7"
+    });
+};
+console.log("connected as to HerokuDB");
 
 module.exports = connection;
